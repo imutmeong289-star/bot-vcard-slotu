d43cb779b5682d219b45063c8a791d7fb46acfcd
imutmeong289-star <imutmeong289@gmail.com>
2026-01-22 16:37:58 +0700

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 3000;
 const BASE_URL = process.env.RENDER_EXTERNAL_URL;
 
 const WEBHOOK_PATH = "/webhook";
-const SHEET_NAME = "DB INCES";
+const SHEET_NAME = "DB SLOTU";
 
 if (!BOT_TOKEN || !SHEET_ID || !GOOGLE_CREDENTIALS || !BASE_URL) {
   console.error("❌ ENV belum lengkap");
